b19267b9915978b11432a0c12207f1f6e718848f
Adithya KP <adithyakpb@gmail.com>
2025-04-10 14:54:11 +0530

    refactor(core): Update core module imports
    - Updated initialization code for core module
    
    - Ensured proper internal imports

--- merit/core/__init__.py
+++ merit/core/__init__.py
@@ -6,5 +6,5 @@ This module provides core functionality for the MERIT framework.
 
 from .logging import get_logger
 from .models import  TestSet, TestItem
-from .utils import parse_json
+from .utils import parse_json, batch_iterator
 from .cache import cache_embeddings, is_caching_available
