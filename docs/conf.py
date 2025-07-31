9fbe41e890e0135dd8fd7c6c01e7eb5937a7618b
adithyakpb <adithyakpb@gmail.com>
2025-08-01 00:13:04 +0530

    feat: update Sphinx configuration with new theme and extensions
    - Change HTML theme to 'furo' for improved aesthetics
    - Add 'sphinx_copybutton' and 'sphinx_search.extension' for enhanced usability
    - Configure theme options for branding and sidebar visibility

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
     'sphinx_rtd_theme',
     'sphinx.ext.autosummary',
     'sphinx_autodoc_typehints',
+    'sphinx_copybutton',
+    'sphinx_search.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +51,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'  # pip install furo
+
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_css_variables": {
+        "color-brand-primary": "#336790",
+        "color-brand-content": "#336790",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
